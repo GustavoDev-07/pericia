72d64c248203e8a51aefe44209dd899aeb99fbce
Emanuella Silva Novais <1046492@senacminas.edu.br>
2026-06-09 15:39:53 -0300

    alteracao_db

--- backend/sql/cliente.sql
+++ backend/sql/cliente.sql
@@ -1,12 +1,12 @@
 USE pericia;
-DROP TABLE IF EXISTS cliente;
-CREATE TABLE cliente(
+DROP TABLE IF EXISTS clientes;
+CREATE TABLE clientes(
 
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(250) NOT NULL,
     email VARCHAR (100),
     cpf_cnpj  VARCHAR(14) NOT NULL UNIQUE,
-    data_de_nascimento VARCHAR(8),
+    data_nascimento VARCHAR(8),
     senha VARCHAR(12)
 
 );
@@ -15,7 +15,7 @@ SELECT
     nome,
     email,
     cpf_cnpj,
-    data_de_nascimento,
+    data_nascimento,
     senha
 FROM
-    cliente;
+    clientes;
