037935e6a56691cc2def61463e2b270e13dd1a9f
Gustavo Enzo Carvalho Norato <carvalhonoratogustavoenzo@gmail.com>
2026-06-15 15:06:57 -0300

    correcao sintaxe

--- backend/sql/cliente.sql
+++ backend/sql/cliente.sql
@@ -5,8 +5,8 @@ CREATE TABLE clientes(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(250) NOT NULL,
     email VARCHAR (100),
-    cpf_cnpj  VARCHAR(14) NOT NULL UNIQUE,
-    data_nascimento VARCHAR(8),
+    cpfCnpj  VARCHAR(14) NOT NULL UNIQUE,
+    dataNascimento VARCHAR(8),
     senha VARCHAR(12)
 
 );
